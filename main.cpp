f68c536635ebb41c93d072b3dad26988146ef33e
Pedro Henrique <pehensil@yahoo.com.br>
2019-04-30 14:52:29 -0300

    Median of three, random pivoing  and time of CPU implemented

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 void printVector(vector<int> &vec){
     for (auto i : vec){
@@ -37,15 +39,33 @@ void swapValues(vector<int> &vec, double shufflePercentage){ // shufflePercentag
 void setPivot(vector<int> &vec, int &pivot, int low, int high, int c){
     switch (c){
         case(1):  // pivot is the last element of partition
-            // return vec.back();
             pivot = vec[high];
             break;
         case(2):  // pivot is the first element of partition
-            pivot = vec[low];
+            swap(vec[low],vec[high]);
+            pivot = vec[high];
             break;
-        case(3):  // pivot is the mean of first, middle and last elements of partition
-            pivot = (vec[low] + vec[high + low - 1] + vec[high]) / 3;
+        case(3):  // pivot is the median of three, middle and last elements of partition
+            int middle;
+            middle = (high + low) / 2;
+            if(vec[middle] < vec[low]){
+                swap(vec[low], vec[middle]);
+            }
+            if(vec[high] < vec[low]){
+                swap(vec[low], vec[high]);
+            }
+            if(vec[middle], vec[high]){
+                swap(vec[middle], vec[high]);
+            }
+            pivot = vec[high];
             break;
+        case(4):  // pivot is a random number of partition
+            swap(vec[low + rand() % (high - low)], vec[high]);
+            pivot = vec[high];
+            break;
+        default:
+            cout << "Pivoting method not found, exiting.\n" << endl;
+            exit(1);
     }
 }
 
@@ -57,8 +77,7 @@ to the left and greaters to the right
 
 int partition(vector<int> &vec, int low, int high){
     int pivot;
-    // TODO not working from parameters (2,3);
-    setPivot(vec, pivot, low, high, 1);
+    setPivot(vec, pivot, low, high, 3);
     int i = (low - 1);  // index of smaller element of actual partition
 
     for (int j = low; j <= high - 1; j++){
@@ -94,10 +113,14 @@ int main()
     vector<int> vec;// = {1,2,3,4,5,5,6,7,8,9};
     int vecSize = 10;
     fillVector(vec, vecSize);
+    swapValues(vec, 1);
     printVector(vec);
-    swapValues(vec, 0.9);
-    printVector(vec);
+    // Calculate the time taken by quickSort
+    auto start = high_resolution_clock::now();
     quickSort(vec, 0, vec.size()-1);
+    auto stop = high_resolution_clock::now();
+    auto timeTaken = duration_cast<microseconds>(stop-start);
+    cout << "Time taken by quickSort: " << timeTaken.count() << "microseconds" << endl;
     printVector(vec);
     return 0;
 }
