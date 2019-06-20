d853988c832bcd68f69f6b2c35f26bcbd821cc61
Pedro Henrique <pehensil@yahoo.com.br>
2019-06-20 12:17:39 -0300

    Script for running tests, finished

--- runTests.sh
+++ runTests.sh
@@ -3,13 +3,13 @@
 totalMethods=4 # Pivoting Methods
 methods=(1 2 3 4)
 
-totalSeeds=30
+totalSeeds=5 # Independent executions
 seeds=(1 2 3 4 5 6 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
 
-totalDimensions=8 # Maybe 7 is enough | for 100000000 took 35674691 microseconds approx
+totalDimensions=2 # 8/7 Maybe 7 is enough | for 100000000 took 35674691 microseconds approx
 dimensions=(10 100 1000 10000 100000 1000000 10000000 100000000)
 
-totalShuffle=3
+totalShuffle=2
 shuffles=(0.1 0.3 0.5)
 
 n=0
@@ -21,7 +21,12 @@ do
 		sh=0
 		while(($sh<$totalShuffle))
 		do
-			./bin/Debug/apaCode -s ${seeds[s]} -n ${dimensions[n]} -p ${shuffles[sh]}
+			m=0
+			while(($m<$totalMethods))
+			do
+				./bin/Debug/apaCode -s ${seeds[s]} -n ${dimensions[n]} -p ${shuffles[sh]} -m ${methods[m]}
+				m=$((m+1))
+			done
 			sh=$((sh+1))
 		done
 		s=$((s+1))
