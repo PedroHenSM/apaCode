14e6656f0aede48cd31e9e042073aaed3b5b82cf
Pedro Henrique <pehensil@yahoo.com.br>
2019-06-28 22:36:41 -0300

    was missing 7 in dimensions vector

--- runTests.sh
+++ runTests.sh
@@ -4,12 +4,12 @@ totalMethods=4 # Pivoting Methods
 methods=(1 2 3 4)
 
 totalSeeds=30 # Independent executions
-seeds=(1 2 3 4 5 6 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
+seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
 
-totalDimensions=2 # 8/7 Maybe 7 is enough | for 100000000 took 35674691 microseconds approx
+totalDimensions=6 # 8/7 Maybe 7 is enough | for 100000000 took 35674691 microseconds approx
 dimensions=(10 100 1000 10000 100000 1000000 10000000 100000000)
 
-totalShuffle=2
+totalShuffle=3
 shuffles=(0.05 0.35 1.1)
 
 n=0
@@ -24,7 +24,7 @@ do
 			m=0
 			while(($m<$totalMethods))
 			do
-				echo "Executing method ${methods[m]} with seed ${seeds[s]} with shufflePercentage ${shuffles[sh]} and dimension ${dimensions[d]}"
+				echo "Executing method ${methods[m]} with seed ${seeds[s]} with shufflePercentage ${shuffles[sh]} and dimension ${dimensions[n]}"
 				./bin/Debug/apaCode -s ${seeds[s]} -n ${dimensions[n]} -p ${shuffles[sh]} -m ${methods[m]}
 				m=$((m+1))
 			done
